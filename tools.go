821a355910453559850def4880614af8d6e6fce7
dotdak <honghung.dotdak@gmail.com>
2023-07-16 21:22:45 +0700

    fix build tools

--- tools.go
+++ tools.go
@@ -1,10 +1,13 @@
-// +build tools
+//go:build tools
 
 package main
 
 import (
+	_ "github.com/envoyproxy/protoc-gen-validate"
+	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
